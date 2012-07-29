6c7de14af21c7c9a84b2546342118e3d8ae5b4d2
Jarl Friis <jarl@softace.dk>
2012-07-29 09:23:37 +0200

    Cleanup and refactoring

--- lib/utf8_enforcer_workaround.rb
+++ lib/utf8_enforcer_workaround.rb
@@ -1,46 +1,19 @@
 require "utf8_enforcer_workaround/version"
-#require 'action_view'
-#require 'action_view/helpers'
 
-#module Utf8EnforcerWorkaround
+module Utf8EnforcerWorkaround
   module ActionView
     module Helpers
       module FormTagHelper
-
-#        extend ActiveSupport::Concern
-#        include ActionView::Helpers::FormTagHelper
-
-        def utf8_enforcer_tag_with_nothing
+        def utf8_enforcer_tag_with_tag_removed
           "".html_safe
         end
-
-        alias_method_chain :utf8_enforcer_tag, :nothing
-         # included do
-         #   puts "aliased"
-         #   alias_method_chain :utf8_enforcer_tag, :nothing
-         # end
       end
     end
   end
- # Your code goes here...
-#end
+end
 
-
-# module Utf8EnforcerWorkaround
-#   module ActionView
-#     module Helpers
-#       module FormTagHelper
-#         extend ActiveSupport::Concern
-# #        include Utf8EnforcerWorkaround::ActionView::Helpers::FormTagHelper
-#       end
-#     end
-#   end
-# end
-
-# ActionView::Helpers::FormTagHelper.class_eval do
-#   include Utf8EnforcerWorkaround::ActionView::Helpers::FormTagHelper
-#   def included(base)
-#     alias_method_chain :utf8_enforcer_tag, :nothing
-#   end
-# end
+ActionView::Helpers::FormTagHelper.class_eval do
+  include Utf8EnforcerWorkaround::ActionView::Helpers::FormTagHelper
+  included { alias_method_chain :utf8_enforcer_tag, :tag_removed }
+end
 
